a5aff42cf549e307a0df92ebbca43f2dd1427f98
Ashwathselvam <ashwathselvam@gmail.com>
2024-01-10 02:53:22 +0530

    added profilleservice template

---
+++ src/IdentityService/Services/CustomerProfileService.cs
@@ -0,0 +1,23 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService;
+
+public class CustomerProfileService : IProfileService
+{
+    public CustomerProfileService(UserManager<ApplicationUser> userManager)
+    {
+
+    }
+    public Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
