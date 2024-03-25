e1c20ba3b90d33a39a90f8b8949f991e869701b1
Ashwathselvam <ashwathselvam@gmail.com>
2024-03-25 18:06:19 +0530

    bid controller

---
+++ src/BiddingService/Controllers/BidsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+
+namespace BiddingService;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BidsController : ControllerBase
+{
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Bid>> PlaceBid(String auctionId, int amount)
+    {
+        var auction = await DB.Find<Auction>().OneAsync(auctionId);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+        if (auction.Seller == User.Identity.Name)
+        {
+            return BadRequest("You cannot Bid on your own auction");
+        }
+
+        var bid = new Bid
+        {
+            Amount = amount,
+            AuctionId = auctionId,
+            Bidder = User.Identity.Name
+        };
+
+        if (auction.AuctionEnd < DateTime.UtcNow)
+        {
+            bid.BidStatus = BidStatus.Finished;
+        }
+    }
+}
